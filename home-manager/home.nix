7a123eaff3e7ebce5ba33e61e942fef198b14b24
salenius <tommisalenius@gmail.com>
2025-03-31 22:48:45 +0300

    Emacs enabled, home-manager switch shortcut created

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -48,9 +48,14 @@
         "ls" = "eza";
         "sl" = "eza";
         "hm" = "vim ~/Projects/salenix/home-manager/home.nix";
+        "hms" = "home-manager switch";
       };
       
       };
+
+    emacs = {
+      enable = true;
+    };
 
     git = {
       enable = true;
