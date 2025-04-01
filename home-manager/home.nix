118b68f721e50aa3776237a4b431896d2e78fdc9
salenius <tommisalenius@gmail.com>
2025-04-01 20:49:03 +0300

    Setting up tmux and alacritty; starship and shell aliases into their own modules.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,14 +1,8 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, ... }@input:
 
 let
   # --- Yhteiset komentojen aliakset shelleille
-  shell-alias-set = {
-        "ls" = "eza";
-        "sl" = "eza";
-        "hm" = "vim ~/Projects/salenix/home-manager/home.nix";
-        "hms" = "home-manager switch";
-      };
-
+  shell-alias-set = import ./modules/shell-aliases.nix;
   # --- Yhteiset rc-tiedostojen komennot (.bashrc, .zshrc jne)
   shell-init-rc-common = ''
        eval "$(zoxide init bash)"
@@ -16,6 +10,11 @@ let
 in
 
 {
+
+  imports = [
+     ./modules/starship.nix
+  ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "tommi";
@@ -50,6 +49,7 @@ in
     #   echo "Hello, ${config.home.username}!"
     # '')
     pkgs.eza
+    pkgs.tmux
   ];
 
   programs = {
@@ -71,6 +71,34 @@ in
       
     };
 
+    tmux = {
+      enable = true;
+      mouse = true;
+      extraConfig = ''
+      set -g visual-bell off
+     '';
+      
+    };
+
+    # Alacritty asetukset kopioitu Eric Murphy (Youtubettaja) esimerkistä
+    alacritty = {
+      enable = true;
+      settings = {
+         window = {
+           padding = { x = 5; y = 5; };
+           class = {
+             instance = "Alacritty";
+             general = "Alacritty";
+           };
+           opacity = 0.75; # 0-1 välillä
+         };
+         scrolling = {
+           history = 10000;
+           multiplier = 3; # Kuinka monta riviä yksi skrollaus etenee
+         };
+      };
+    };
+
 
     emacs = {
       enable = true;
@@ -85,87 +113,8 @@ in
       };
     };
 
-    starship =
-     let
-      programming-language-format = "[[ $symbol  ]]()";
-      programming-symbol = smbol: {
-          symbol = smbol;
-          style = "bg:#212736";
-          format = programming-language-format;
-        };
-      # ----- Kansiopolkujen värit -----
-      iron-color = "#e3e5e5"; # Alkuperäinen polun väri, itselle liian tumma taustaan nähden
-      white-color = "#fdfdfd";
-
-     in
-     {
-      enable = true;
-      settings = {
-        "$schema" = "https://starship.rs/config-schema.json";
-        add_newline = true;
-        aws.disabled = true;
-        gcloud.disabled = true;
-        line_break.disabled = true;
-        format = lib.concatStrings [
-        "[░▒▓](#a3aed2)"
-        "[  ](bg:#a3aed2 fg:#090c0c)"
-        "[](bg:#769ff0 fg:#a3aed2)"
-        "$directory"
-        "[](fg:#769ff0 bg:#394260)"
-        "$git_branch"
-        "$git_status"
-        "[](fg:#394260 bg:#212736)"
-        "$nodejs"
-        "$rust"
-        "$golang"
-        "$php"
-        "[](fg:#212736 bg:#1d2230)"
-        "$time"
-        "[ ](fg:#1d2230)"
-        "$nix_shell"
-        "\n$character"
-        ];
-        directory = {
-          style = "fg:${white-color} bg:#769ff0";
-          format = "[ $path ]($style)";
-          truncation_length = 3;
-          truncation_symbol = "…/";
-          substitutions = {
-            "Documents" = "󰈙 ";
-            "Downloads" = " ";
-            "Music" = " ";
-            "Pictures" = " ";
-          };
-        };
-        git_branch = {
-          symbol = "";
-          style = "bg:#394260";
-          format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
-        };
-        git_status = {
-          style = "bg:#394260";
-          format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
-        };
-        time = {
-          disabled = false;
-          time_format = "%R"; # Hour:Minute Format
-          style = "bg:#1d2230";
-          format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
-        };
-        nix_shell = {
-          disabled = false;
-          format = "[$symbol$state](bold blue) nix-shell";
-        };
-
-        rust = programming-symbol "";
-        golang = programming-symbol "";
-        php = programming-symbol "";
-       
-
-      };
-    };
-  }; 
-
+  };
+     
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
@@ -199,8 +148,10 @@ in
   #
   home.sessionVariables = {
     # EDITOR = "emacs";
+    TERMINAL = "alacritty";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
 }
