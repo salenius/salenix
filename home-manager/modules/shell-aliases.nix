b7c8d54eb768e3c2638680f42d15c3b5267c1678
salenius <tommisalenius@gmail.com>
2025-04-23 22:04:48 +0300

    Lisätty aliaksia useille komennoille (eza, devour emacs, xclip jne)

--- home-manager/modules/shell-aliases.nix
+++ home-manager/modules/shell-aliases.nix
@@ -1,9 +1,23 @@
 {
-  "ls" = "eza";
-  "sl" = "eza";
-  "hm" = "vim ~/Projects/salenix/home-manager/home.nix";
+  "ls"  = "eza --color=always --group-directories-first --icons";
+  "ll"  = "eza -la --icons --octal-permissions --group-directories-first";
+  "l"   = "eza -bGF --header --git --color=always --group-directories-first --icons";
+  "llm" = "eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons"; 
+  "la"  = "eza --long --all --group --group-directories-first";
+  "lx"  = "eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons";
+
+  "sl" = "eza --color=always --group-directories-first --icons";
   "hms" = "home-manager switch";
   "cat" = "bat";
+  "e" = "devour emacs";
+  "conf" = "devour emacs ~/Projects/salenix/nixos/configuration.nix";
+  "skriptit" = "devour emacs ~/Projects/salenix/nixos/skriptit.nix";
+  "home" = "devour emacs ~/Projects/salenix/home-manager/home.nix";
+  "hm" = "home";
+  "clipboard" = "xclip -selection clipboard"; # Tällä saadaan kopioitua leikepöydälle
+  "clip" = "clipboard";
+  "aliases" = "e ~/Projects/salenix/home-manager/modules/shell-aliases.nix";
+  "rangr" = "devour rangr";
 }
 
 
